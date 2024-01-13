6a921fa2eb44a7e4fc64b275a816164dd25475ed
VM5Ball <vladimir-markovich@mail.ru>
2024-01-14 02:41:19 +0300

    test commit

--- react-portfolio-1/src/App.jsx
+++ react-portfolio-1/src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
       <About />
       <Experience />
       <Services />
-      <Portfolio />
+      {/* <Portfolio /> */}
       <Achievements />
       <Contact />
       <Footer />
