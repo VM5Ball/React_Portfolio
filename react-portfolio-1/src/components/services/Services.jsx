6108a7d08168330328e3bf276399f38004588a3a
VM5Ball <vladimir-markovich@mail.ru>
2024-01-14 13:03:36 +0300

    service update

--- react-portfolio-1/src/components/services/Services.jsx
+++ react-portfolio-1/src/components/services/Services.jsx
@@ -2,79 +2,28 @@ import React from 'react'
 import './services.css'
 import {TiTick} from 'react-icons/ti'
 
-const data_python = [
-  {
-    skill: 'Принципы ООП'
-  },
-  {
-    skill: 'Работы с данными в Pandas'
-  },
-  {
-    skill: 'Обработка данных в NumPy'
-  },
-  {
-    skill: 'SQLAlchemy'
-  },
-  {
-    skill: 'Построение гистограмм'
-  },
-  {
-    skill: 'Функции агрегации'
-  },
-  
-  
-  
-]
-
-const data_job = [
-  {
-    skill: 'Автоматизация отчётности'
-  },
-  {
-    skill: 'Проверки качества данных'
-  },
-  {
-    skill: 'Анализ распределений'
-  },
-  {
-    skill: 'Очистка данных'
-  },
-  {
-    skill: 'Feature engineering'
-  },
-  {
-    skill: 'Анализ рисков ипотечных активов'
-  },
-  
-  
-  
+const data_dbm = [
+  {skill: 'ETL'},
+  {skill: 'Query performance'},
+  {skill: 'SQLAlchemy'},
 ]
 
 const data_ML = [
-  {
-    skill: 'Линейная регрессия'
-  },
-  {
-    skill: 'Метод градиентного спуска'
-  },
-  {
-    skill: 'Классификация'
-  },
-  {
-    skill: 'Логистическая регрессия'
-  },
-  {
-    skill: 'Метод опорных векторов'
-  },
-  {
-    skill: 'Линейный перцептрон'
-  },
-  {
-    skill: 'Свёрточные нейронные сети'
-  },
-  {
-    skill: 'Задачи CV и NLP'
-  },
+  {skill: 'PyTorch'},
+  {skill: 'Sci-kit Learn'},
+  {skill: 'Time Series Forecasting'},
+  {skill: 'Linear models'},
+  {skill: 'LightGBM'},
+  {skill: 'XGBoost'},
+  {skill: 'CatBoost'},
+]
+
+const data_reporting = [
+  {skill: 'Grafana'},
+  {skill: 'Superset'},
+  {skill: 'Matplotlib'},
+  {skill: 'Plotly Express'},
+  {skill: 'Seaborn'},
 ]
 
 const Services = () => {
@@ -87,11 +36,11 @@ const Services = () => {
       <div className="container services__container">
         <article className="service">
           <div className="service__head">
-            <h3>Python & SQL</h3>
+            <h3>DBM</h3>
           </div>
           <ul className="service__list">
             {
-              data_python.map(({skill}) => {
+              data_dbm.map(({skill}) => {
                 return (
                   <li>
                     <TiTick className='service__list-icon'/>
@@ -105,25 +54,7 @@ const Services = () => {
 
         <article className="service">
           <div className="service__head">
-            <h3>Аналитика</h3>
-          </div>
-          <ul className="service__list">
-            {
-              data_job.map(({skill}) => {
-                return (
-                  <li>
-                    <TiTick className='service__list-icon'/>
-                    <p>{skill}</p>
-                  </li> 
-                )
-              })
-            } 
-          </ul>
-        </article>
-
-        <article className="service">
-          <div className="service__head">
-            <h3>ML</h3>
+            <h3>Python</h3>
           </div>
           <ul className="service__list">
             {
@@ -139,10 +70,23 @@ const Services = () => {
           </ul>
         </article>
 
-        
-
-        
-
+        <article className="service">
+          <div className="service__head">
+            <h3>Reporting</h3>
+          </div>
+          <ul className="service__list">
+            {
+              data_reporting.map(({skill}) => {
+                return (
+                  <li>
+                    <TiTick className='service__list-icon'/>
+                    <p>{skill}</p>
+                  </li> 
+                )
+              })
+            } 
+          </ul>
+        </article>
       </div>
 
       </section>
